a7b5ac2ca9eb6a3a6846c9786835d3028485da84
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-21 09:50:45 -0300

    Criando o projeto Order Service

---
+++ delivery/delivery-order-service/src/main/java/com/coderef/delivery/DeliveryOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryOrderServiceApplication.class, args);
+	}
+}
